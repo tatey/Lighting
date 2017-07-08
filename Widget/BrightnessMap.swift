0b682d928660df10e7bcb4c27b8a5d0db4997169
Ern <segan5@gmail.com>
2017-07-08 20:49:33 +1000

    Use light colour scheme for widget and expanded selection of white values

--- Widget/BrightnessMap.swift
+++ Widget/BrightnessMap.swift
@@ -8,11 +8,22 @@ import LIFXHTTPKit
 
 struct ColorMap {
 	enum Key {
-		case hotWhite
-		case warmWhite
-		case coolWhite
-		case coldWhite
-		case blueWhite
+        case ultraWarm
+        case incandescent
+        case warm
+        case neutralWarm
+        case neutral
+        case cool
+        case coolDaylight
+        case softDaylight
+        case daylight
+        case noonDaylight
+        case brightDaylight
+        case cloudyDaylight
+        case blueDaylight
+        case blueOvercast
+        case blueWater
+        case blueIce
 		case red
 		case orange
 		case yellow
@@ -34,19 +45,30 @@ struct ColorMap {
 	}
 
 	fileprivate static let Dictionary: [Key: Value] = [
-		.hotWhite:  Value(description: "Hot White",  value: Color.white(2500)),
-		.warmWhite: Value(description: "Warm White", value: Color.white(3500)),
-		.coolWhite: Value(description: "Cool White", value: Color.white(4500)),
-		.coldWhite: Value(description: "Cold White", value: Color.white(5500)),
-		.blueWhite: Value(description: "Blue White", value: Color.white(9000)),
-		.red:       Value(description: "Red",        value: Color.color(0,   saturation: 1)),
-		.orange:    Value(description: "Orange",    value: Color.color(40,  saturation: 1)),
-		.yellow:    Value(description: "Yellow",     value: Color.color(60,  saturation: 1)),
-		.green:     Value(description: "Green",      value: Color.color(120, saturation: 1)),
-		.cyan:      Value(description: "Cyan",       value: Color.color(180, saturation: 1)),
-		.blue:      Value(description: "Blue",       value: Color.color(240, saturation: 1)),
-		.purple:    Value(description: "Purple",     value: Color.color(280, saturation: 1)),
-		.pink:      Value(description: "Pink",       value: Color.color(320, saturation: 1)),
+        .ultraWarm:     Value(description: "Ultra Warm 2500K", value: Color.white(2500)),
+        .incandescent:  Value(description: "Incandescent 2750K", value: Color.white(2750)),
+        .warm:          Value(description: "Warm 3000K", value: Color.white(3000)),
+        .neutralWarm:   Value(description: "Neutral Warm 3200K", value: Color.white(3200)),
+        .neutral:       Value(description: "Neutral 3500K", value: Color.white(3500)),
+        .cool:          Value(description: "Cool 4000K", value: Color.white(4000)),
+        .coolDaylight:  Value(description: "Cool Daylight 4500K", value: Color.white(4500)),
+        .softDaylight:  Value(description: "Soft Daylight 5000K", value: Color.white(5000)),
+        .daylight:      Value(description: "Daylight 5500K", value: Color.white(5500)),
+        .noonDaylight:  Value(description: "Noon Daylight 6000K", value: Color.white(6000)),
+        .brightDaylight: Value(description: "Bright Daylight 6500K", value: Color.white(6500)),
+        .cloudyDaylight: Value(description: "Cloudy Daylight 7000K", value: Color.white(7000)),
+        .blueDaylight:  Value(description: "Blue Daylight 7500K", value: Color.white(7500)),
+        .blueOvercast:  Value(description: "Blue Overcast 8000K", value: Color.white(8000)),
+        .blueWater:     Value(description: "Blue Water 8500K", value: Color.white(8500)),
+        .blueIce:       Value(description: "Blue Ice 9000K", value: Color.white(9000)),
+		.red:           Value(description: "Red",        value: Color.color(0,   saturation: 1)),
+		.orange:        Value(description: "Orange",    value: Color.color(40,  saturation: 1)),
+		.yellow:        Value(description: "Yellow",     value: Color.color(60,  saturation: 1)),
+		.green:         Value(description: "Green",      value: Color.color(120, saturation: 1)),
+		.cyan:          Value(description: "Cyan",       value: Color.color(180, saturation: 1)),
+		.blue:          Value(description: "Blue",       value: Color.color(240, saturation: 1)),
+		.purple:        Value(description: "Purple",     value: Color.color(280, saturation: 1)),
+		.pink:          Value(description: "Pink",       value: Color.color(320, saturation: 1)),
 	]
 
 	static func valueForKey(_ key: Key) -> Value {
